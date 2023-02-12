9166b2b21ead027b1074b0a5aabdf7504c20f8c3
RogenBenastra <info@tuagir.ru>
2023-02-12 06:25:59 +0300

    attempt #3

--- Updater/1compile Updater.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-javac -encoding UTF8 -Xdiags:verbose Updater.java 2>err.txt
-set fldr=Z:\java\Durak\
-for /R %fldr% %%I In (err.txt) do if %%~zI equ 0 del /F /Q %%~I
-if EXIST err.txt start /min /b ..\\Audio.exe Sounds\error.wav
