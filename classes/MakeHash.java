c62bae78d01bf85f6d28a6919b313795ad533172
RogenBenastra <info@tuagir.ru>
2023-02-14 19:17:50 +0300

    refactoring code

--- classes/MakeHash.java
+++ classes/MakeHash.java
@@ -5,6 +5,7 @@ import java.security.MessageDigest;
 
 public class MakeHash
 {
+
 public static String getHash(String fileName) {
 FileInputStream inputStream = null;
 try {
@@ -36,37 +37,6 @@ e.printStackTrace();
 }
 }
 }
-}
 
-
-/*
-  public static String getHash(String fileName)
-{
-try{    
-MessageDigest digest = MessageDigest.getInstance("SHA-256");
-    FileInputStream inputStream = new FileInputStream(fileName);
-    byte[] data = new byte[1024];
-    int read = 0;
-    while ((read = inputStream.read(data)) != -1)
-{
-      digest.update(data, 0, read);
-    }
-    byte[] hash = digest.digest();
-    StringBuilder hexString = new StringBuilder();
-    for (byte b : hash)
-{
-      String hex = Integer.toHexString(0xff & b);
-      if (hex.length() == 1)
-{
-        hexString.append('0');
 }
-            hexString.append(hex);
-}
-        return hexString.toString();
-} catch (Exception e) {
-        return e.getMessage();
-    }
- }//fn
-*/
-
 }//cl
